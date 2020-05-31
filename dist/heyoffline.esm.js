848290496c4348d8dab794c0471a3369f35cbaa5
Oskar Krawczyk <oskar@krawczyk.io>
2020-05-31 23:35:54 +0200

    Added rollup to build ESM, UMD and CJS

--- dist/heyoffline.esm.js
+++ dist/heyoffline.esm.js
@@ -1,10 +1,7 @@
-// TODO:
-// - module integration
-
-class Heyoffline {
+const Heyoffline = class Heyoffline {
 
   constructor(options){
-    this.version = "2.0.0"
+    this.version = "2.0.0";
 
     // default options
     this.options = {
@@ -25,10 +22,10 @@ class Heyoffline {
       fields:         "input, select, textarea, *[contenteditable]"
       // onOnline:  () => {},
       // onOffline: () => {}
-    }
+    };
 
     // set a global flag if any field on the page has been modified
-    this.modified = false
+    this.modified = false;
 
     this.defaultStyles = `
       .${this.options.prefix}_overlay {
@@ -103,13 +100,13 @@ class Heyoffline {
         border: solid 1px rgba(255, 255, 255, 0.6);
         color: rgba(255, 255, 255, 0.9);
       }
-    `
+    `;
 
-    this.online      = this.online.bind(this)
-    this.offline     = this.offline.bind(this)
-    this.hideMessage = this.hideMessage.bind(this)
-    this.options     = this.extend(this.options, options)
-    this.setup()
+    this.online      = this.online.bind(this);
+    this.offline     = this.offline.bind(this);
+    this.hideMessage = this.hideMessage.bind(this);
+    this.options     = this.extend(this.options, options);
+    this.setup();
   }
 
   // extend object with another objects
@@ -117,10 +114,10 @@ class Heyoffline {
     if (source) {
       for (let property in source) {
         if (source[property] && source[property].constructor && (source[property].constructor === Object)) {
-          destination[property] = destination[property] || {}
-          arguments.callee(destination[property], source[property])
+          destination[property] = destination[property] || {};
+          arguments.callee(destination[property], source[property]);
         } else {
-          destination[property] = source[property]
+          destination[property] = source[property];
         }
       }
     }
@@ -131,7 +128,7 @@ class Heyoffline {
     this.events = {
       fields:  ["keyup", "change"],
       network: ["online", "offline"]
-    }
+    };
 
     this.elements = {
       fields:  document.querySelectorAll(this.options.fields),
@@ -142,21 +139,21 @@ class Heyoffline {
       heading: document.createElement("strong"),
       desc:    document.createElement("p"),
       button:  document.createElement("a")
-    }
+    };
 
-    this.attachEvents()
+    this.attachEvents();
 
     if (!this.noStyles){
-      this.addStyles()
+      this.addStyles();
     }
   }
 
   addStyles(){
-    let style = document.createElement("style")
-    style.innerHTML = this.defaultStyles
+    let style = document.createElement("style");
+    style.innerHTML = this.defaultStyles;
 
-    let ref = document.querySelector("body")
-    ref.parentNode.insertBefore(style, ref)
+    let ref = document.querySelector("body");
+    ref.parentNode.insertBefore(style, ref);
   }
 
   createElements(){
@@ -167,10 +164,10 @@ class Heyoffline {
       element: "overlay",
       onCreate: (el) => {
         if (!this.options.overlay){
-          el.style["pointer-events"] = "none"
+          el.style["pointer-events"] = "none";
         }
       }
-    })
+    });
 
     // modal
     this.newElement({
@@ -179,122 +176,124 @@ class Heyoffline {
       className: "hidden",
       onCreate: (el) => {
         setTimeout(() => {
-          el.classList.remove("hidden")
-        }, 200)
+          el.classList.remove("hidden");
+        }, 200);
       }
-    })
+    });
 
     // icon
     this.newElement({
       context: this.elements.modal,
       element: "icon"
-    })
+    });
 
     // content
     this.newElement({
       context: this.elements.modal,
       element: "content"
-    })
+    });
 
     // heading
     this.newElement({
       context: this.elements.content,
       element: "heading"
-    })
+    });
 
     // description
     this.newElement({
       context: this.elements.content,
       element: "desc"
-    })
+    });
 
     // button
     if (!this.options.disableDismiss){
       this.newElement({
         context: this.elements.modal,
         element: "button"
-      })
-      this.elements.button.addEventListener("click", this.hideMessage)
+      });
+      this.elements.button.addEventListener("click", this.hideMessage);
     }
   }
 
   newElement(options){
-    let el = this.elements[options.element]
+    let el = this.elements[options.element];
 
-    el.setAttribute("class", `${this.options.prefix}_${options.element}`)
-    el = options.context.appendChild(el)
+    el.setAttribute("class", `${this.options.prefix}_${options.element}`);
+    el = options.context.appendChild(el);
 
     if (options.className){
-      el.classList.add(options.className)
+      el.classList.add(options.className);
     }
 
     if (this.options.text.offline[options.element]){
-      el.innerHTML = this.options.text.offline[options.element]
+      el.innerHTML = this.options.text.offline[options.element];
     }
 
     if (options.onCreate){
-      options.onCreate.call(this, el)
+      options.onCreate.call(this, el);
     }
   }
 
   destroyElements(){
     if (this.elements.overlay){
-      this.elements.overlay.remove()
+      this.elements.overlay.remove();
     }
   }
 
   attachEvents(){
     for (let field of Array.from(this.elements.fields)){
-      this.elementEvents(field)
+      this.elementEvents(field);
     }
 
     for (let event of Array.from(this.events.network)){
-      this.networkEvents(event)
+      this.networkEvents(event);
     }
   }
 
   elementEvents(field){
     Array.from(this.events.fields).map((event) =>
       field.addEventListener(event, () => {
-        this.modified = true
+        this.modified = true;
       })
-    )
+    );
   }
 
   networkEvents(event){
-    window.addEventListener(event, this[event])
+    window.addEventListener(event, this[event]);
   }
 
   online(event){
-    this.hideMessage()
+    this.hideMessage();
   }
 
   offline(){
     if (this.options.monitorFields){
       if (this.modified){
-        this.showMessage()
+        this.showMessage();
       }
     } else {
-      this.showMessage()
+      this.showMessage();
     }
   }
 
   showMessage(){
-    this.createElements()
+    this.createElements();
 
     if (this.options.onOnline){
-      this.options.onOnline.call(this)
+      this.options.onOnline.call(this);
     }
   }
 
   hideMessage(event){
     if (event){
-      event.preventDefault()
+      event.preventDefault();
     }
 
-    this.destroyElements()
+    this.destroyElements();
     if (this.options.onOffline){
-      this.options.onOffline.call(this)
+      this.options.onOffline.call(this);
     }
   }
-}
+};
+
+export default Heyoffline;
